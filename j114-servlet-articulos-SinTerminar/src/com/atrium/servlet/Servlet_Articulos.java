28bf49550c4aec1e14e4bbe7dd1596c2ed4e1d86
Raúl <raul.hernandez@testeandosoftware.com>
2014-04-22 12:07:19 +0200

    Añadidos escuchadores de eventos

---
+++ j114-servlet-articulos-SinTerminar/src/com/atrium/servlet/Servlet_Articulos.java
@@ -0,0 +1,32 @@
+package com.atrium.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet_Articulos extends HttpServlet {
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest peticion, HttpServletResponse respuesta)
+			throws ServletException, IOException {
+		
+		String codigo_articulo=peticion.getParameter("codigo_articulo");
+		
+
+		
+	}
+
+}
