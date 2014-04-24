353990c43750feb518a6214a92d1ce3635a874cf
Raúl <raul.hernandez@testeandosoftware.com>
2014-04-24 13:19:23 +0200

    Añadidos nuevos ejercicios

--- j114-servlet-articulos-raul/src/com/atrium/etiquetas/comboproveedores.java
+++ j114-servlet-articulos-raul/src/com/atrium/etiquetas/comboproveedores.java
@@ -1,16 +1,21 @@
 package com.atrium.etiquetas;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
 public class comboproveedores extends SimpleTagSupport {
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		// TODO Auto-generated method stub
+		super.doTag();
+	}
 	
 	// COLECCION PARA IDIOMATIZAR LAS OPCIONES DE MENU
-		private List<Lista_Proveedores> lista_proveedores;
-		private ResourceBundle rb;
-	
 	// RECOJO LA LISTA DE PROVEEDORES
 	// PIDO LA FACHADA AL APPLICATION CONTEXT DE SPRING
 	// CREAMOS POR PRIMERA VEZ LA COLECCION DE PROVEEDORES
--- j114-servlet-articulos-raul/src/com/atrium/hibernate/modelo/Gestion_Proveedores.java
+++ j114-servlet-articulos-raul/src/com/atrium/hibernate/modelo/Gestion_Proveedores.java
@@ -3,21 +3,16 @@ package com.atrium.hibernate.modelo;
 import com.atrium.hibernate.dao.ProveedoresDAO;
 
 public class Gestion_Proveedores implements IGestion_Proveedor {
-	
+
 	private ProveedoresDAO proveedor_dao;
 
-	/* (non-Javadoc)
-	 * @see com.atrium.hibernate.modelo.IGestion_proveedor#getProveedor_dao()
-	 */
-	@Override
-	public ProveedoresDAO getProveedor_dao() {
-		return proveedor_dao;
+	public List<Proveedores> consultar_Todos() {
+		return proveedor_dao.findAll();
 	}
 
 	public void setProveedor_dao(ProveedoresDAO proveedor_dao) {
 		this.proveedor_dao = proveedor_dao;
 	}
-	
-	
+
 
 }
